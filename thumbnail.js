6d8b942ed69145705b3532833b844be03a418644
Ehann <39786933+Ehann@users.noreply.github.com>
2021-06-19 23:42:19 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,14 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
     let targetImage = document.querySelector("#smart-image");
+    targetImage.addEventListener("click", function() {
+        alert("点击了图片");
+        targetImage.classList.remove("small");
 
-targetImage.addEventListener("click", function() {
-alert("点击了图片");
-if (targetImage.classList.contains("small")) {
-    targetImage.classList.remove("small");
-} else {
-    targetImage.classList.add("small");
-}
-    
-});
 });
