afb1b6c54ac2418c9a53de9978a84739ce9c7527
Ehann <39786933+Ehann@users.noreply.github.com>
2021-06-20 10:11:09 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -5,11 +5,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 targetImage.addEventListener("click", function() {
 alert("点击了图片");
-if (targetImage.classList.contains("small")) {
-    targetImage.classList.remove("small");
-} else {
+
     targetImage.classList.add("small");
-}
-    
-});
+
 });
